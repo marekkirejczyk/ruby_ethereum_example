8497ce745c2e2cecac42fe85496c0a2ebc9fd0b4
Marek <marek.kirejczyk@gmail.com>
2017-01-29 14:19:53 +0100

    add boostrap and minimal styling

--- app/controllers/greeters_controller.rb
+++ app/controllers/greeters_controller.rb
@@ -2,7 +2,6 @@ class GreetersController < ApplicationController
 
   def index
     path = "#{Dir.pwd}/contracts/greeter.sol" 
-    contract = Ethereum::Contract.create(file: path, address: "0x7ff1875dc0e62ab1459688bdbe3f3df112f2097e")
-    render text: contract.call.greet
+    @contract = Ethereum::Contract.create(file: path, address: "0x7ff1875dc0e62ab1459688bdbe3f3df112f2097e")
   end
 end
